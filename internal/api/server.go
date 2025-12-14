457b6acdea18a9a30f10756eb940023d729713c4
seeques <equestrianbuddy@gmail.com>
2025-12-14 15:30:48 +0300

    Add storage.postgresStorage to Server struct

--- internal/api/server.go
+++ internal/api/server.go
@@ -4,19 +4,28 @@ import (
 	"net/http"
 	"context"
 
+	"github.com/rs/zerolog/log"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/seeques/task-api/internal/config"
 	"github.com/seeques/task-api/internal/handler"
+	"github.com/seeques/task-api/internal/storage"
 )
 
 type Server struct {
 	httpServer *http.Server
 	router chi.Router
+	postgresStorage *storage.PostgresStorage
 }
 
 func NewServer() *Server {
 	cfg := config.LoadConfig()
+	pool, err := storage.CreatePool()
+	if err != nil {
+		log.Fatal().Err(err).Msg("Pool creation failed")
+	}
+
+	pgStorage := storage.NewPostgresStorage(pool)
 
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
@@ -30,6 +39,7 @@ func NewServer() *Server {
 	return &Server{
 		httpServer: server,
 		router: r,
+		postgresStorage: pgStorage,
 	}
 }
 
@@ -39,4 +49,8 @@ func (s *Server) Start() {
 
 func (s *Server) Shutdown(ctx context.Context) {
 	s.httpServer.Shutdown(ctx)
+}
+
+func (s *Server) ClosePool() {
+	s.postgresStorage.ClosePool()
 }
