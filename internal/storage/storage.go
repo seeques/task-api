f046d0a2e90f94f7355042c7af3897a213062022
seeques <equestrianbuddy@gmail.com>
2025-12-14 18:43:31 +0300

    Implement SaveUser function of Storage interface

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -11,6 +11,19 @@ type PostgresStorage struct {
 	pool *pgxpool.Pool
 }
 
+type Storage interface {
+	SaveUser(ctx context.Context, usr *User) error
+}
+
+func (s *PostgresStorage) SaveUser(ctx context.Context, usr *User) error {
+	sql := `INSERT INTO users (email, password_hash)
+	VALUES ($1, $2)
+	RETURNING id, created_at`
+
+	_, err := s.pool.QueryRow(ctx, sql, usr.Email, usr.PasswordHash).Scan(&usr.ID, &usr.CreatedAt)
+	return err
+}
+
 func NewPostgresStorage(pool *pgxpool.Pool) *PostgresStorage {
 	return &PostgresStorage{pool: pool}
 }
