4f6f4f7d567940e03f2bcba493fccca616caa3ef
seeques <equestrianbuddy@gmail.com>
2025-12-14 11:38:27 +0300

    Add graceful shutdown logic

--- main.go
+++ main.go
@@ -3,18 +3,38 @@ package main
 import (
 	"net/http"
 	"encoding/json"
+	"os"
+	"os/signal"
+	"syscall"
+	"context"
+	"time"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
-	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
-		Health(w, r)
-	})
-	http.ListenAndServe(":8080", r)
+	r.Get("/health", Health)
+
+	server := &http.Server{
+		Addr: ":8080",
+		Handler: r,
+	}
+
+	go func() {
+		server.ListenAndServe()
+	}()
+
+	<-sigs
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	server.Shutdown(ctx)
 }
 
 func Health(w http.ResponseWriter, r *http.Request) {
