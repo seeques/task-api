fb94fc4b782978cf6ae5c1744b98751f7f61c721
seeques <equestrianbuddy@gmail.com>
2025-12-15 12:51:52 +0300

    Make health function a method on Handler

--- internal/handler/health.go
+++ internal/handler/health.go
@@ -5,7 +5,7 @@ import (
 	"encoding/json"
 )
 
-func Health(w http.ResponseWriter, r *http.Request) {
+func (h *Handler) Health(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(map[string]string{"status": "ok"})
