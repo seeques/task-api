d9e131eb0126c8c6617adb2af9f18774df70e004
seeques <equestrianbuddy@gmail.com>
2025-12-15 19:55:09 +0300

    Add login related functionality

--- internal/handler/auth.go
+++ internal/handler/auth.go
@@ -1,28 +1,36 @@
 package handler
 
 import (
-	"net/http"
 	"encoding/json"
+	"net/http"
 	"strings"
 
 	"github.com/rs/zerolog/log"
-	"github.com/seeques/task-api/internal/response"
 	"github.com/seeques/task-api/internal/auth"
+	"github.com/seeques/task-api/internal/response"
 	"github.com/seeques/task-api/internal/storage"
+	"github.com/seeques/task-api/internal/config"
 )
 
 type RegisterRequest struct {
-	Email string `json:"email"`
+	Email    string `json:"email"`
 	Password string `json:"password"`
+}
+
+type LoginRequest struct {
+	Email    string `json:"email"`
+	Password string `json:password`
 }
 
 type Handler struct {
 	storage *storage.PostgresStorage
+	cfg *config.Config
 }
 
-func NewHandler(storage *storage.PostgresStorage) *Handler {
+func NewHandler(storage *storage.PostgresStorage, cfg *config.Config) *Handler {
 	return &Handler{
 		storage: storage,
+		cfg: cfg,
 	}
 }
 
@@ -52,7 +60,7 @@ func (h *Handler) Register(w http.ResponseWriter, r *http.Request) {
 	}
 
 	usr := &storage.User{
-		Email: req.Email,
+		Email:        req.Email,
 		PasswordHash: hash,
 	}
 
@@ -64,4 +72,39 @@ func (h *Handler) Register(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-type", "application/json")
 	w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(usr)
+}
+
+func (h *Handler) Login(w http.ResponseWriter, r *http.Request) {
+	var req LoginRequest
+
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		response.RespondError(w, http.StatusBadRequest, "invalid JSON")
+		return
+	}
+
+	usr, err := h.storage.GetUserByEmail(r.Context(), req.Email)
+	if err != nil {
+		response.RespondError(w, http.StatusInternalServerError, "database error")
+		return
+	}
+	if usr == nil {
+		response.RespondError(w, http.StatusUnauthorized, "invalid credentials")
+		return
+	}
+
+	if err := auth.CheckPassword(req.Password, usr.PasswordHash); err != nil {
+    	response.RespondError(w, http.StatusUnauthorized, "invalid credentials")
+    	return
+	}
+
+	token, err := auth.GenerateToken(usr.ID, h.cfg.JWTsecret)
+	if err != nil {
+		response.RespondError(w, http.StatusInternalServerError, "token creation error")
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(map[string]string{"token": token})
 }
