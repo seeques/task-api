42192419ee1ca8606b415fc03f9cdf031c0540cb
seeques <equestrianbuddy@gmail.com>
2025-12-15 12:49:34 +0300

    Update package naming

---
+++ internal/auth/password.go
@@ -0,0 +1,16 @@
+package auth
+
+import (
+	"golang.org/x/crypto/bcrypt"
+)
+
+func HashPassword(password string) (string, error) {
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+
+	if err != nil {
+		return "", err
+	}
+
+	hashString := string(hash)
+	return hashString, nil
+}
