677313b226edd39795671f7a1c7709c848578a0a
seeques <equestrianbuddy@gmail.com>
2025-12-16 12:00:37 +0300

    Add JWT token validation function

--- internal/auth/jwt.go
+++ internal/auth/jwt.go
@@ -2,6 +2,7 @@ package auth
 
 import (
 	"time"
+	"errors"
 
 	"github.com/golang-jwt/jwt/v5"
 )
@@ -24,4 +25,25 @@ func GenerateToken(userID int, secret string) (string, error) {
 	ss, err := token.SignedString([]byte(secret))
 
 	return ss, err
+}
+
+func ValidateToken(tokenString, secret string) (int, error) {
+	token, err := jwt.ParseWithClaims(tokenString, &CustomClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte(secret), nil
+	})
+
+	if err != nil {
+		return 0, err
+	}
+
+	if !token.Valid {
+		return 0, errors.New("invalid token")
+	}
+
+	claims, ok := token.Claims.(*CustomClaims)
+	if !ok {
+		return 0, errors.New("invalid claims")
+	}
+
+	return claims.ID, nil
 }
