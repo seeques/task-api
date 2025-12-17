0cf7ef9b8a06c6c48c4fc4a4a8c42fb44cbd3b66
seeques <equestrianbuddy@gmail.com>
2025-12-17 10:55:55 +0300

    Fix query sql statement

--- internal/storage/task.go
+++ internal/storage/task.go
@@ -36,20 +36,19 @@ func (s *PostgresStorage) CreateTask(ctx context.Context, task *Task) error {
 }
 
 func (s *PostgresStorage) GetTask(ctx context.Context, taskID int, userID int) (*Task, error) {
-	query := `SELECT * FROM tasks WHERE id = $1 AND user_id = $2
-	RETURNING title, description, completed, created_at, updated_at`
+	query := `SELECT id, user_id, title, description, completed, created_at, updated_at 
+    FROM tasks WHERE id = $1 AND user_id = $2`
 
-	task := Task{
-		ID: taskID,
-		UserID: userID,
-	}
+	task := Task{}
 
-	err := s.pool.QueryRow(ctx, query, taskID, userID).Scan(
-		&task.Title, 
-		&task.Description, 
-		&task.Completed, 
-		&task.CreatedAt, 
-		&task.UpdatedAt,
-	)
+    err := s.pool.QueryRow(ctx, query, taskID, userID).Scan(
+        &task.ID,
+        &task.UserID,
+        &task.Title, 
+        &task.Description, 
+        &task.Completed, 
+        &task.CreatedAt, 
+        &task.UpdatedAt,
+    )
 	return &task, err
 } 
