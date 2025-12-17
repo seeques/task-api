e6dcae5a63e212e8660238ea79d541ff23cbe957
seeques <equestrianbuddy@gmail.com>
2025-12-17 11:35:58 +0300

    Add getter for all taska pertained to user_id

--- internal/storage/task.go
+++ internal/storage/task.go
@@ -51,4 +51,39 @@ func (s *PostgresStorage) GetTask(ctx context.Context, taskID int, userID int) (
         &task.UpdatedAt,
     )
 	return &task, err
-} 
+}
+
+func (s *PostgresStorage) ListTasks(ctx context.Context, userID int) ([]Task, error) {
+	query := `SELECT id, user_id, title, description, completed, created_at, updated_at 
+    FROM tasks WHERE user_id = $1`
+
+	rows, err := s.pool.Query(ctx, query, userID)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var tasks []Task
+	for rows.Next() {
+		var task Task
+		err := rows.Scan(
+		&task.ID,
+        &task.UserID,
+        &task.Title, 
+        &task.Description, 
+        &task.Completed, 
+        &task.CreatedAt, 
+        &task.UpdatedAt,
+		)
+		if err != nil {
+			return nil, err
+		}
+		tasks = append(tasks, task)
+	}
+
+	if err := rows.Err(); err != nil {
+        return nil, err
+    }
+
+	return tasks, nil
+}
