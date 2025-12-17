7575ca35a50cb19cf102484e7593944ce9112934
seeques <equestrianbuddy@gmail.com>
2025-12-17 10:56:20 +0300

    Add task getter function

--- internal/handler/task.go
+++ internal/handler/task.go
@@ -3,13 +3,15 @@ package handler
 import (
 	"encoding/json"
 	"net/http"
+	"strconv"
 
-	"github.com/seeques/task-api/internal/storage"
+	"github.com/go-chi/chi/v5"
 	"github.com/seeques/task-api/internal/response"
+	"github.com/seeques/task-api/internal/storage"
 )
 
 type CreateTaskRequest struct {
-	Title string `json:"title"`
+	Title       string `json:"title"`
 	Description string `json:"description"`
 }
 
@@ -29,8 +31,8 @@ func (h *Handler) CreateTask(w http.ResponseWriter, r *http.Request) {
 	}
 
 	task := &storage.Task{
-		UserID: userID,
-		Title: req.Title,
+		UserID:      userID,
+		Title:       req.Title,
 		Description: req.Description,
 	}
 
@@ -42,5 +44,31 @@ func (h *Handler) CreateTask(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(task)
+}
+
+func (h *Handler) GetTask(w http.ResponseWriter, r *http.Request) {
+	userID := storage.GetUserID(r)
+
+	idStr := chi.URLParam(r, "id")
+    id, err := strconv.Atoi(idStr)
+	if err != nil {
+		response.RespondError(w, http.StatusNotFound, "task not found")
+		return
+	}
+
+	if id == 0 {
+		response.RespondError(w, http.StatusBadRequest, "task id is required")
+		return
+	}
+
+	task, err := h.storage.GetTask(r.Context(), id, userID)
+	if err != nil {
+		response.RespondError(w, http.StatusInternalServerError, "unable to get the task")
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(task)
 }
